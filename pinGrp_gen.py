a4e0a283475ce9dc70583b846b30604b5720964f
jgwei2009 <jgwei1983@gmail.com>
2023-07-03 12:05:45 -0700

    add capability to remove rectangular shaped pin groups to form donut shaped groups

--- pinGrp_gen.py
+++ pinGrp_gen.py
@@ -6,6 +6,18 @@ dim1_step_per_net = 2
 dim2_st = 14
 dim2_ed = 50
 dim2_step_per_net = 2
+
+##### remove pins from pin group def, to obtain a donut shape pin group, if desired 
+is_enable_rm = False  
+
+rm_dim1_st = "M"
+rm_dim1_ed = "AM"
+rm_dim1_step_per_net = 2 
+
+rm_dim2_st = 20
+rm_dim2_ed = 40
+rm_dim2_step_per_net = 2
+
 ##### End of User Input 
 
 ##### Do Check with your layout to make sure dim1_list assumption is valid 
@@ -23,8 +35,22 @@ for i_dim1 in range ( dim1_st_idx, dim1_ed_idx + 1, dim1_step_per_net):
     for i_dim2 in range (dim2_st, dim2_ed + 1, dim2_step_per_net):
         newPin = dim1_list[i_dim1] + str(i_dim2)
         pinList.append( newPin )
-        
-print(pinList)
+
+print("INFO: pin group list (before rm) : ", pinList)
+
+if is_enable_rm:
+    rm_dim1_st_idx = dim1_list.index(rm_dim1_st)
+    rm_dim1_ed_idx = dim1_list.index(rm_dim1_ed)
+    print('INFO: rm dim1 index start: ' + str(rm_dim1_st_idx) + ', end: '+ str(rm_dim1_ed_idx) + '\n')
+
+    for i_dim1 in range ( rm_dim1_st_idx, rm_dim1_ed_idx + 1, rm_dim1_step_per_net):
+        for i_dim2 in range (rm_dim2_st, rm_dim2_ed + 1, rm_dim2_step_per_net):
+            newPin = dim1_list[i_dim1] + str(i_dim2)
+            #print('##### debug: ' + newPin)
+            pinList.remove( newPin )    
+            
+    print("INFO: pin group list final : ", pinList)
+
 
 
 
